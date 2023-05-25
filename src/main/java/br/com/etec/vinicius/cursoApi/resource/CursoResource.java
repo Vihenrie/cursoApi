50e5a03097fb490ad5336324be5ec98dbc935a5e
Vihenrie <viniciushpintolp@gmail.com>
2023-05-25 08:53:14 -0300

    Update CursoResource.java

--- src/main/java/br/com/etec/vinicius/cursoApi/resource/CursoResource.java
+++ src/main/java/br/com/etec/vinicius/cursoApi/resource/CursoResource.java
@@ -2,7 +2,11 @@ package br.com.etec.vinicius.cursoApi.resource;
 
 import br.com.etec.vinicius.cursoApi.model.Curso;
 import br.com.etec.vinicius.cursoApi.repository.CursoRepository;
+import br.com.etec.vinicius.cursoApi.repository.filter.CursoFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +18,12 @@ public class CursoResource {
 
     @Autowired
     private CursoRepository cursoRepository;
+
+    @GetMapping()
+    public Page<Curso> pesquisar(CursoFilter cursoFilter, Pageable pageable) {
+        return cursoRepository.Filtrar(cursoFilter, pageable);
+    }
+
 
     @RequestMapping("/todos")
     public List<Curso> ListarTodosCursos() {
