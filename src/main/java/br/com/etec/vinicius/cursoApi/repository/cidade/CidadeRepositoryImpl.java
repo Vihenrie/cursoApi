8887cbab808cbe15cc0850bc103e64588bdb11d8
Vihenrie <viniciushpintolp@gmail.com>
2023-06-01 08:56:43 -0300

    Cidade Query completo

--- src/main/java/br/com/etec/vinicius/cursoApi/repository/cidade/CidadeRepositoryImpl.java
+++ src/main/java/br/com/etec/vinicius/cursoApi/repository/cidade/CidadeRepositoryImpl.java
@@ -1,4 +1,82 @@
 package br.com.etec.vinicius.cursoApi.repository.cidade;
 
+
+import br.com.etec.vinicius.cursoApi.model.Cidade;
+import br.com.etec.vinicius.cursoApi.repository.filter.CidadeFilter;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class CidadeRepositoryImpl implements CidadeRepositoryQuery{
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public Page<Cidade> Filtrar(CidadeFilter cidadeFilter, Pageable pageable) {
+
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Cidade> criteria = builder.createQuery(Cidade.class);
+        Root<Cidade> root = criteria.from(Cidade.class);
+
+        Predicate[] predicates = criarRestricoes(cidadeFilter, builder, root);
+        criteria.where(predicates);
+        criteria.orderBy(builder.asc(root.get("nomecidade")));
+
+        TypedQuery<Cidade> query = manager.createQuery(criteria);
+        adicionarRestricoesDePaginacao(query, pageable);
+
+        return new PageImpl<>(query.getResultList(), pageable, total(cidadeFilter));
+    }
+
+    private Long total(CidadeFilter cidadeFilter) {
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Long> criteria = builder.createQuery(Long.class);
+        Root<Cidade> root = criteria.from(Cidade.class);
+
+        Predicate[] predicates = criarRestricoes(cidadeFilter, builder, root);
+        criteria.where(predicates);
+        criteria.orderBy(builder.asc(root.get("nomecidade")));
+
+        criteria.select(builder.count(root));
+
+        return  manager.createQuery(criteria).getSingleResult();
+    }
+
+    private void adicionarRestricoesDePaginacao(TypedQuery<Cidade> query, Pageable pageable) {
+        int paginaAtual = pageable.getPageNumber();
+        int totRegPorPag = pageable.getPageSize();  //total de Registros Por Pagina
+        int primeRegPag = paginaAtual * totRegPorPag; //primeiro Registro da Pagina
+
+        query.setFirstResult(primeRegPag);
+        query.setMaxResults(totRegPorPag);
+    }
+
+    private Predicate[] criarRestricoes(CidadeFilter cidadeFilter, CriteriaBuilder builder, Root<Cidade> root) {
+
+        List<Predicate> predicates = new ArrayList<>();
+
+        if(!StringUtils.isEmpty(cidadeFilter.getNomecidade())){
+            predicates.add(builder.like(builder.lower(root.get("nomecidade")),
+                    "%" + cidadeFilter.getNomecidade().toLowerCase() + "%"));
+        }
+        if(!StringUtils.isEmpty(cidadeFilter.getUf())){
+            predicates.add(builder.like(builder.lower(root.get("nomeuf")),
+                    "%" + cidadeFilter.getUf().toLowerCase() + "%"));
+        }
+        return predicates.toArray(new Predicate[predicates.size()]);
+    }
+
 }
--- src/main/java/br/com/etec/vinicius/cursoApi/repository/cidade/CidadeRepositoryQuery.java
+++ src/main/java/br/com/etec/vinicius/cursoApi/repository/cidade/CidadeRepositoryQuery.java
@@ -1,4 +1,12 @@
 package br.com.etec.vinicius.cursoApi.repository.cidade;
 
+import br.com.etec.vinicius.cursoApi.model.Cidade;
+import br.com.etec.vinicius.cursoApi.repository.filter.CidadeFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface CidadeRepositoryQuery {
+
+    public Page<Cidade> Filtrar(CidadeFilter cidadeFilter, Pageable pageable);
+
 }
